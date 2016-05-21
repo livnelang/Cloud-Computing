bc99cfc99d2a23a5529e30fcae019102b0b84c6a
Livne Lang <livnelang@gmail.com>
2016-05-21 14:18:30 +0300

    seperated client and & ws

--- client/js/controllers/picturepanel.controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('picturePanelCtrl',['$scope','$stateParams','categoriesFactory', function ($scope, $stateParams, categoriesFactory) {
-	$scope.picture = categoriesFactory.currentPicture;
-    console.dir(categoriesFactory.currentPicture);
-
-}]) ;
-
